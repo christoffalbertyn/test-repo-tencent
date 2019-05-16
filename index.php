0659e808e7aa74a6b2ada0792dc22c626e543f7c
Christoff <christoffalbertyn@NicholaesiPhone.localdomain>
2019-05-16 10:15:49 +0200

    DSTV API

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+//how to connect to a secret service on Tencent
+$secretKey = 'TenAcentsDD4ec-45332XyZ';
+$userName = 'SuperSecretAdmin';
+
+//Use these values to connect to the database on 8.8.8.8
